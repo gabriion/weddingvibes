ab254987f3e93c67ea421f36a49c3b0ebf9cf57b
gabriion <gabriel_gion@yahoo.com>
2019-12-26 21:26:23 +0200

    implementing test send email

---
+++ contact-form-handler.php
@@ -0,0 +1,35 @@
+<?php
+$errors = '';
+$myemail = 'gabriel_gion@yahoo.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  ||
+   empty($_POST['email']) ||
+   empty($_POST['guest']) ||
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$guest = $_POST['guest']);
+$message = $_POST['message'];
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+{
+$to = $myemail;
+$email_subject = "Confirmare nunta: $name";
+$email_body = "You have received a new message. ".
+"Here are the details:\n Name: $name \n , attending with $guest guests".
+"Email: $email_address\n Message \n $message";
+$headers = "From: $myemail\n";
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+//redirect to the 'thank you' page
+header('Location: index.html');
+}
+?>
